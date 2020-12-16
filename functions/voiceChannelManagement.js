776d709e880eca11d7b6c63bcbf54da9f4cbfe56
burn-piano-island <burn_piano_island@outlook.com>
2020-12-16 15:18:26 -0800

    Adding permissions to auto-created voice channels

--- functions/voiceChannelManagement.js
+++ functions/voiceChannelManagement.js
@@ -12,7 +12,24 @@ module.exports.createVoiceChannelForMember = (state) => {
 
         //TODO: if the user's channel already exists, just put them in that and prevent deletion
         // create channel for user
-        guild.channels.create(user_channel_name, {type: VOICE_TYPE, parent: category_channel.id}).then(channel => {
+        guild.channels.create(user_channel_name, {
+            //voice channel
+            type: VOICE_TYPE,
+            //parent category
+            parent: category_channel.id,
+            //permissions
+            permissionOverwrites: [
+                //allow user to move members out of this channel
+                {
+                    id: user.id,
+                    allow: [
+                        'MOVE_MEMBERS',
+                        'MUTE_MEMBERS',
+                        'DEAFEN_MEMBERS'
+                    ]
+                }
+            ]
+        }).then(channel => {
             // add user to channel
             state.member.voice.setChannel(channel);
         });
