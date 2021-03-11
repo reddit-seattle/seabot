fe59a187ae94f68ec6b98263d879e494f9ebdb18
burn-piano-island <burn_piano_island@outlook.com>
2021-03-11 15:27:45 -0800

    fixed auto-deletion logic in rant channel

--- functions/deleto.js
+++ functions/deleto.js
@@ -2,13 +2,13 @@
 const constants = require('../utils/constants');
 module.exports.delete = async (message) => {
     try{
-    tenPrev = await message.channel.messages.fetch({before: message.id, limit: 11});
-    if(tenPrev.size < 11) {
+    msgs = await message.channel.messages.fetch({before: message.id, limit: 11});
+    if(msgs.size < 11) {
         return;
     }
-    last = tenPrev.array()[tenPrev.size-1];
-    prev = (await message.channel.messages.fetch({before: last.id, limit:50}));
-    message.channel.bulkDelete(prev.array(), true);
+    msg = msgs.array()[10];
+    previous = (await message.channel.messages.fetch({before: msg.id, limit:50}));
+    message.channel.bulkDelete(previous.array(), true);
     }
     catch(e){
         console.dir(e);
