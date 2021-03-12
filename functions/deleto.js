3506d302cf72d31d1eb44c3230c13c7ac8d6dce8
burn-piano-island <burn_piano_island@outlook.com>
2021-03-11 16:53:33 -0800

    Rant channel cron job to clear channel after an hour of inactivity

--- functions/deleto.js
+++ functions/deleto.js
@@ -1,5 +1,7 @@
 
 const constants = require('../utils/constants');
+const _ = require('underscore');
+
 module.exports.delete = async (message) => {
     try{
     msgs = await message.channel.messages.fetch({before: message.id, limit: 11});
@@ -15,3 +17,22 @@ module.exports.delete = async (message) => {
     }
 
 };
+
+module.exports.deleteOldMessages = async (client) => {
+    _.each(client.guilds.cache.array(), async (guild) => {
+        const rantChannel = guild.channels.cache.find(ch => ch.name == 'rant');
+        last = await rantChannel.messages.fetch({limit: 1});
+        if(!last.array()?.[0]) {
+            return;
+        }
+        msg = last.array()[0];
+        hours_since = (new Date() - msg.createdAt) / 1000 / 60 / 60;
+        if(hours_since > 1){
+            //last message is over an hour old, kill it all
+            previous = (await rantChannel.messages.fetch({limit:50}));
+            rantChannel.bulkDelete(previous.array(), true);
+            rantChannel.send('Messages here will be deleted as new messages are typed. The channel will be cleared if no messages are sent within one hour.');
+
+        }
+    });
+}
