9455d16f1dd5793f35663356f60c982c92edc331
Zenith <z@zenith.dev>
2021-06-20 22:26:33 -0700

    dm new accounts (<5 mins) on join (#35)
    * dm new accounts (<5 mins) on join
    
    * bro what
    
    * Apply suggestions from code review
    
    Co-authored-by: burn-piano-island <36089446+burn-piano-island@users.noreply.github.com>

--- src/commands/joinLeaveCommands.ts
+++ src/commands/joinLeaveCommands.ts
@@ -6,3 +6,9 @@ export const abeLeaves = (member: GuildMember | PartialGuildMember) => {
         guild?.systemChannel?.send('https://media.giphy.com/media/fDO2Nk0ImzvvW/giphy.gif')
     }
 }
+
+export const newAccountJoins = (member: GuildMember | PartialGuildMember) => {
+    if(((new Date()).getTime() - member.user!.createdTimestamp) < 1000 * 60 * 5) {
+        member.send(`Hey ${member.user!.username} - just a reminder, your account needs to be at least 5 minutes old to chat. While you wait, feel free to browse our welcome channel for some basic rules and channel descriptions.`)
+    }
+}
