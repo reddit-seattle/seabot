36028ed550bef17db65374b88b279a4b2d7ab997
Zenith <z@zenith.dev>
2021-06-21 23:56:14 -0700

    muh style

--- src/commands/joinLeaveCommands.ts
+++ src/commands/joinLeaveCommands.ts
@@ -8,7 +8,7 @@ export const abeLeaves = (member: GuildMember | PartialGuildMember) => {
 }
 
 export const newAccountJoins = (member: GuildMember | PartialGuildMember) => {
-    if(((new Date()).getTime() - member.user!.createdTimestamp) < 1000 * 60 * 5) {
+    if (((new Date()).getTime() - member.user!.createdTimestamp) < 1000 * 60 * 5) {
         member.send(`Hey ${member.user!.username} - just a reminder, your account needs to be at least 5 minutes old to chat. While you wait, feel free to browse our welcome channel for some basic rules and channel descriptions.`)
     }
 }
