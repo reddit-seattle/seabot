ae2b1a41613e37c7e137ccdbe0b832fbbc3bed09
burn-piano-island <36089446+burn-piano-island@users.noreply.github.com>
2021-06-28 10:31:36 -0700

    Update joinLeaveCommands.ts

--- src/commands/joinLeaveCommands.ts
+++ src/commands/joinLeaveCommands.ts
@@ -1,14 +1,19 @@
 import { GuildMember, PartialGuildMember } from "discord.js";
 
+const FIVE_MINUTES = 1000 * 60 * 5;
+
 export const abeLeaves = (member: GuildMember | PartialGuildMember) => {
-    if (((new Date()).getTime() - member.joinedAt?.getTime()!) < 1000 * 60 * 5) {
+    if (Date.now() - member.joinedAt?.getTime()! < FIVE_MINUTES) {
         const { guild } = member;
         guild?.systemChannel?.send('https://media.giphy.com/media/fDO2Nk0ImzvvW/giphy.gif')
     }
 }
 
 export const newAccountJoins = (member: GuildMember | PartialGuildMember) => {
-    if(((new Date()).getTime() - member.user!.createdTimestamp) < 1000 * 60 * 5) {
-        member.send(`Hey ${member.user!.username} - just a reminder, your account needs to be at least 5 minutes old to chat. While you wait, feel free to browse our welcome channel for some basic rules and channel descriptions.`)
+    if (Date.now() - member.user!.createdTimestamp < FIVE_MINUTES) {
+        member.send(`
+            Hey ${member.user!.username} - just a reminder, your account needs to be at least 5 minutes old to chat. 
+            While you wait, feel free to browse our welcome channel for some basic rules and channel descriptions.`
+        )
     }
 }
