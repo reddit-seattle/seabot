69121e89c4facc429d100e715c0f8eecf1d8b9cf
burn-piano-island <36089446+burn-piano-island@users.noreply.github.com>
2022-01-22 22:49:43 -0800

    Remove discord handler - next-auth handles this now

--- src/http_handlers/discordHandler.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-
-export const discordAuth = async (req: Request, res: Response) => {
-    const { code, guild_id } = req?.query;
-    res.end('wip')
-}
