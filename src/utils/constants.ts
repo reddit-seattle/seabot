f67d43b25ac4c36c9e245031a236e136c8341c6b
burn-piano-island <36089446+burn-piano-island@users.noreply.github.com>
2022-06-02 19:00:25 -0700

    Minor bugfixes
    Fixed pluralization of days since an incident
    
    Added missing link param to incident creation
    
    Added optional custom secondary text to whoops command

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -85,14 +85,14 @@ export module Strings {
     export const coffee = '`HTTP ERR: 418 - I am a teapot`';
     export const newIssueURL = 'https://github.com/reddit-seattle/seabot/issues/new/choose';
     export const feedbackText = 'See an issue? Want to request a feature?';
-    export const whoops = (text: string, emoji: string) => `
+    export const whoops = (text: string, emoji: string, bottomtext?: string) => `
     whoops
     ⊂ヽ
     　 ＼＼ ${text}
     　　 ＼( ͡° ͜ʖ ͡°)
     　　　 >　⌒ヽ
     　　　/ 　 へ＼
-    　　 /　　/　＼＼fell out
+    　　 /　　/　＼＼${bottomtext ?? 'fell out'}
     　　 ﾚ　ノ　　 ヽつ
     　　/　) )
     　 /　/|　💦 
