91d2a9f2a17be5d0842e1b5120a0fd3cece2a933
Dameon Laird <dameonlair@gmail.com>
2022-12-02 11:44:30 -0800

    Now uses in memory database if Cosmos cannot be found.

--- src/commands/slash/database/DatabaseCommand.ts
+++ src/commands/slash/database/DatabaseCommand.ts
@@ -1,22 +1,25 @@
-import { Command, CommandConfiguration } from "../../Command";
+import { CosmosClient, ItemDefinition } from "@azure/cosmos";
+
 import DBConnector from "../../../db/DBConnector";
+import IDatabase from "../../../db/IDatabase";
+import InMemoryDbConnector from "../../../db/InMemoryDbConnector";
+import SlashCommand, { SlashCommandConfiguration } from "../SlashCommand";
+
 import { Database } from "../../../utils/constants";
 import { cosmosClient } from "../../../db/cosmosClient";
-import { CosmosClient, ItemDefinition } from "@azure/cosmos";
-import InMemoryDbConnector from "../../../db/InMemoryDbConnector";
-import IDatabase from "../../../db/IDatabase";
 
 type ConnectorType = "Awards" | "Incidents" | "MessageTelemetry";
 
-export class DatabaseCommand<ModelType extends ItemDefinition> extends Command {
+export class DatabaseCommand<ModelType extends ItemDefinition> extends SlashCommand {
     private static connectorCache = new Map<string, any>();
+
     public static getConnector<ModelType extends ItemDefinition>(connectorType: ConnectorType) {
         if (DatabaseCommand.connectorCache.has(connectorType)) {
             return DatabaseCommand.connectorCache.get(connectorType);
         }
 
         let connector: IDatabase<ModelType>;
-        if (process.env.DEBUG) {
+        if (!cosmosClient) {
             connector = new InMemoryDbConnector<ModelType>();
         } else {
             connector = new DBConnector<ModelType>(cosmosClient as CosmosClient, Database.DATABASE_ID, connectorType);
@@ -31,12 +34,17 @@ export class DatabaseCommand<ModelType extends ItemDefinition> extends Command {
     }
 
     private _connector;
+
     public get connector() {
         return this._connector;
     }
 
-    constructor(connectorType: ConnectorType, configuration: CommandConfiguration) {
+    constructor(connectorType: ConnectorType, configuration: SlashCommandConfiguration) {
         super(configuration);
         this._connector = DatabaseCommand.getConnector<ModelType>(connectorType);
+    }
+
+    public execute(...args: any[]) {
+        super.execute(args);
     }
 }
