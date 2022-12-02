88be7be2e4f9476fd97aaaed5ab52a5b059008ea
Dameon Laird <dameonlair@gmail.com>
2022-12-02 11:27:12 -0800

    Re-implimented custom string matching. Reaction trigger now created dynamically.

--- src/commands/messageContent/AutoReact.ts
+++ src/commands/messageContent/AutoReact.ts
@@ -1,39 +1,73 @@
 import { Message } from "discord.js";
 
-import { replaceMentions } from "../../utils/helpers";
-import { discordBot } from "../../server";
+import ContentCommand from "./ContentCommand";
 
-const reactionMap = new Map<string | RegExp, string>([
-    [/hodor/i, "door"],
-    [/bisbopt/i, "bisbopt"],
-    [/duck/i, "duck"],
-    [/69/i, "nice"],
-    [/420/i, "weed"],
-    [/puya[1ilӏ]{1,2}up/i, "downvote"],
-    [/bruh/i, "bruh"]
+import { discordBot } from "../../server";
+import { replaceMentions } from "../../utils/helpers";
+import { REGEX } from "../../utils/constants";
+
+type AutoReaction = {
+    emojiName: string;
+    trim?: boolean;
+    enableLinks?: boolean;
+};
+
+const reactionMap = new Map<string | RegExp, AutoReaction>([
+    [/hodor/i, { emojiName: "door" }],
+    [/bisbopt/i, { emojiName: "bisbopt" }],
+    [/duck/i, { emojiName: "duck" }],
+    [/69/i, { emojiName: "nice", trim: true }],
+    [/420/i, { emojiName: "weed", trim: true }],
+    [/puya[1ilӏ]{1,2}up/i, { emojiName: "downvote" }],
+    [/bruh/i, { emojiName: "bruh" }],
 ]);
 
-export default {
-    trigger: /(hodor|bisbopt|duck|69|420|puya[1ilӏ]{1,2}up|bruh)/i,
-    handler: (message: Message) => {
-        message.content = replaceMentions(message);
-        for (const [ trigger, reaction ] of reactionMap) {
-            if ((trigger instanceof RegExp)) {
-                if (!trigger.test(message.content)) {
-                    continue;
-                }
+const trigger = new RegExp(
+    [...reactionMap.keys()]
+        .map((key) => {
+            if (typeof key === "string") {
+                return key;
             } else {
-                if (!message.content.toLowerCase().includes(trigger)) {
-                    continue;
-                }
+                return (key as RegExp).source;
+            }
+        })
+        .join("|"),
+    "i"
+);
+
+export default new ContentCommand({
+    name: "autoreact",
+    description: "Automatically reacts to messages when a certain string or regex is triggered.",
+    trigger,
+    handler: (message: Message) => {
+        for (const [trigger, reaction] of reactionMap) {
+            if (!shouldReactToMessage(trigger, reaction)) continue;
+
+            const emoji = discordBot.client.emojis.cache.find((x) => x.name === reaction.emojiName);
+            if (!emoji) {
+                throw new Error(`Emoji for autoreact "${reaction.emojiName}" cannot be found.`);
             }
 
-            const emoji = discordBot.client.emojis.cache.find(x => x.name === reaction);
-            if (!emoji) {
-                throw new Error(`Emoji for autoreact "${reaction}" cannot be found.`);
-            }
-            
             message.react(emoji);
+            // One reaction per customer, please.
+            break;
         }
-    }
-}
+
+        function shouldReactToMessage(trigger: string | RegExp, reaction: AutoReaction) {
+            let sanitizedMessage = replaceMentions(message).toLowerCase();
+            if (!reaction.enableLinks) {
+                sanitizedMessage = sanitizedMessage.replace(REGEX.URL, "");
+            }
+
+            if (reaction.trim) {
+                sanitizedMessage = sanitizedMessage.replace(/s+/g, "");
+            }
+
+            if (trigger instanceof RegExp) {
+                return trigger.test(sanitizedMessage);
+            } else {
+                return sanitizedMessage.includes(trigger);
+            }
+        }
+    },
+});
