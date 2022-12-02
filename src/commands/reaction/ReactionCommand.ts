e0d5a94b29e56f1b4ac28f3755784e37c433108c
Dameon Laird <dameonlair@gmail.com>
2022-12-02 11:34:44 -0800

    Now stores configuration rather than copying individual fields. Generalized execute/canExecute.

--- src/commands/reaction/ReactionCommand.ts
+++ src/commands/reaction/ReactionCommand.ts
@@ -1,21 +1,36 @@
+import { MessageReaction, User } from "discord.js";
+
 import { Command, CommandConfiguration } from "../Command";
 
 export interface ReactionCommandConfiguration extends CommandConfiguration {
     removeReaction?: boolean;
     emojiName?: string;
+    execute: (reaction: MessageReaction, user: User) => any | Promise<any>;
 }
 
 export default class ReactionCommand extends Command {
-    private _emojiName: string;
-    public get emojiName() { return this._emojiName; }
-    private _removeReaction: boolean;
-    public get removeReaction() {
-        return this._removeReaction;
+    private _configuration: ReactionCommandConfiguration;
+
+    constructor(configuration: ReactionCommandConfiguration) {
+        super(configuration);
+        this._configuration = configuration;
     }
 
-    constructor(config: ReactionCommandConfiguration) {
-        super(config);
-        this._removeReaction = config.removeReaction ?? false;
-        this._emojiName = config.emojiName ?? config.name;
+    public get emojiName() {
+        return this._configuration.emojiName;
+    }
+
+    public get removeReaction() {
+        return this._configuration.removeReaction;
+    }
+
+    public canExecute(...args: any[]) {
+        return true;
+    }
+
+    public execute(...args: any[]): any {
+        const reaction = args[0];
+        const user = args[1];
+        this._configuration.execute(reaction, user);
     }
 }
