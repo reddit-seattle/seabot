84f591c34ae6b15e1511d5891057e96959b8b15f
Dameon Laird <dameonlair@gmail.com>
2022-12-02 11:31:12 -0800

    Removed error handling (errors should be caught at higher leve). Moved functionality out of SlashCommandRouter.

--- src/commands/reaction/CreateServerEvent.ts
+++ src/commands/reaction/CreateServerEvent.ts
@@ -1,32 +1,40 @@
 import {
-    DiscordAPIError,
     GuildScheduledEvent,
     GuildScheduledEventCreateOptions,
-    Message,
-    PartialMessage,
+    GuildScheduledEventPrivacyLevel,
+    MessageReaction,
+    User,
 } from "discord.js";
-import { parseApolloMarkdownLink, pullTimeStampsFromApolloString } from "../../utils/helpers";
+
 import ReactionCommand from "./ReactionCommand";
+
+import { Environment, UserIDs } from "../../utils/constants";
+import { isModReaction, parseApolloMarkdownLink, pullTimeStampsFromApolloString } from "../../utils/helpers";
 
 export default new ReactionCommand({
     emojiName: "calendar",
     removeReaction: true,
     adminOnly: true,
     name: "schedule",
-    help: `allows mods to create server events out of apollo messages`,
     description: `allows mods to create server events out of apollo messages`,
-    execute: async (message: Message | PartialMessage): Promise<GuildScheduledEvent | undefined> => {
-        const embed = message.embeds?.[0];
+    execute: async (reaction: MessageReaction, user: User): Promise<GuildScheduledEvent | undefined> => {
+        const message = reaction.message;
+        //dangerous - allow seabot to react to a bot's commands for creating server events
+        if (message.author?.id !== UserIDs.APOLLO || Environment.DEBUG || !isModReaction(reaction, user)) {
+            return;
+        }
+
+        const embed = reaction.message.embeds?.[0];
         const { title, description, image, fields, footer } = embed;
 
         // check for duplicate event
-        const allEvents = await message.guild?.scheduledEvents.fetch();
+        const allEvents = await reaction.message.guild?.scheduledEvents.fetch();
         const dupes = allEvents?.filter((event) => event.name == title);
         if (dupes?.size) {
             return;
         }
 
-        const { url } = message;
+        const { url } = reaction.message;
 
         const time = fields[0].value; //<t:timestamp:F>\n<:countdown:emojiId> <t:timestamp:R>
         const { start, end } = pullTimeStampsFromApolloString(time);
@@ -34,17 +42,16 @@ export default new ReactionCommand({
         const calendarLink = parseApolloMarkdownLink(fields[1].value); // [add to calendar](link)
         const { title: calendarTitle, url: calendarUrl } = calendarLink;
 
-        try {
-            return (await message.guild?.scheduledEvents.create({
-                entityType: 3, // "EXTERNAL",
-                name: title?.substring(0, 100), // limited to 100 characters
-                privacyLevel: 2, // "GUILD_ONLY",
-                scheduledStartTime: start,
-                scheduledEndTime: end,
-                image: image?.url,
-                description: `${description?.substring(0, 300)}${
-                    description?.length && description?.length > 300 ? "..." : ""
-                }
+        const event = (await reaction.message.guild?.scheduledEvents.create({
+            entityType: 3, // "EXTERNAL",
+            name: title?.substring(0, 100), // limited to 100 characters
+            privacyLevel: GuildScheduledEventPrivacyLevel.GuildOnly,
+            scheduledStartTime: start,
+            scheduledEndTime: end,
+            image: image?.url,
+            description: `${description?.substring(0, 300)}${
+                description?.length && description?.length > 300 ? "..." : ""
+            }
 
 ${calendarTitle}:
 ${calendarUrl}
@@ -52,15 +59,14 @@ ${calendarUrl}
 Event link: ${url}
 
 ${footer?.text}`,
-                entityMetadata: {
-                    location: "See event link in description",
-                },
-                reason: `Auto-created event from bot react: ${url}`,
-            } as GuildScheduledEventCreateOptions)) as GuildScheduledEvent;
-        } catch (err: any) {
-            if (err instanceof DiscordAPIError) {
-                console.dir(err);
-            }
-        }
+            entityMetadata: {
+                location: "See event link in description",
+            },
+            reason: `Auto-created event from bot react: ${url}`,
+        } as GuildScheduledEventCreateOptions)) as GuildScheduledEvent;
+        //confirm event creation
+        await message.react("✅");
+        //attempt to message user who created it with link
+        await user.send(`Event created: ${event.url}`);
     },
 });
