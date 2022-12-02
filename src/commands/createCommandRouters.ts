e088645d5815769472790fedbc91938b907542f2
Dameon Laird <dameonlair@gmail.com>
2022-12-02 11:14:41 -0800

    Split different types of command routers into their own files

---
+++ src/commands/createCommandRouters.ts
@@ -0,0 +1,16 @@
+import commands from "./";
+import ContentCommandRouter from "./messageContent/ContentCommandRouter";
+import DiscordBot from "../discord/DiscordBot";
+import DiscordEventRouter from "../discord/DiscordEventRouter";
+import ReactionCommandRouter from "./reaction/ReactionCommandRouter";
+import SlashCommandRouter from "./slash/SlashCommandRouter";
+
+export default function createCommandRouters(eventRouter: DiscordEventRouter, discordBot: DiscordBot) {
+    const routers = [
+        new ContentCommandRouter(eventRouter, discordBot, commands.content),
+        new ReactionCommandRouter(eventRouter, discordBot, commands.reaction),
+        new SlashCommandRouter(eventRouter, discordBot, commands.slash),
+    ];
+
+    return routers;
+}
