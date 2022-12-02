103bd35892ce9b0366aedffcc3d8bf6b7109e025
Dameon Laird <dameonlair@gmail.com>
2022-12-02 09:42:56 -0800

    Converted to abstract class

--- src/commands/Command.ts
+++ src/commands/Command.ts
@@ -5,42 +5,31 @@ export type SlashCommandResult = () => Omit<SlashCommandBuilder, "addSubcommand"
 
 export interface CommandConfiguration {
     name: string;
-    emojiName?: string;
     description: string;
-    help: string;
-    slashCommandDescription?: SlashCommandResult;
-    executeSlashCommand?: SlashCommandHandler;
+    help?: string;
     adminOnly?: boolean;
-    execute?: Function;
 }
 
-//TODO - expand interface to have a 'canExecute' method to check args and return help message
-export class Command {
-    private _name: string;
-    public get name() { return this._name; }
-    private _adminOnly = true;
-    public get adminOnly() { return this._adminOnly; }
-    private _description: string;
-    public get description() { return this._description; }
-    private _help: string;
-    public get help() { return this._help; }
-    private _slashCommandDescription?: SlashCommandResult;
-    async slashCommandDescription() { return await this._slashCommandDescription?.call(this); }
-    private _executeSlashCommand?: SlashCommandHandler;
-    async executeSlashCommand(interaction: ChatInputCommandInteraction) { return await this._executeSlashCommand?.call(this, interaction); }
-    private _execute?: Function;
-    get execute() { return this._execute; }
+export abstract class Command {
+    protected configuration: CommandConfiguration;
+
+    public abstract canExecute(...args: any[]): any;
+    public abstract execute(...args: any[]): any;
+
+    public get name() {
+        return this.configuration.name;
+    }
+    public get adminOnly() {
+        return this.configuration.adminOnly;
+    }
+    public get description() {
+        return this.configuration.description;
+    }
+    public get help() {
+        return this.configuration.help ?? this.configuration.description;
+    }
 
     constructor(configuration: CommandConfiguration) {
-        this._name = configuration.name;
-        this._description = configuration.description;
-        this._help = configuration.help;
-        this._adminOnly = configuration.adminOnly ?? false;
-        this._slashCommandDescription = configuration.slashCommandDescription ?? undefined;
-        this._executeSlashCommand = configuration.executeSlashCommand ?? undefined;
-        this._execute = configuration.execute;
+        this.configuration = configuration;
     }
-    
 }
-
-export interface CommandDictionary { [id: string]: Command }
