1bd9f2bd282c0513f005b66c527ed98969f7c8a5
Dameon Laird <dameonlair@gmail.com>
2022-12-06 11:28:46 -0800

    Moved configuration files to their own folder. Extracted loadConfiguration from server.ts.

---
+++ src/configuration/defaultConfig.ts
@@ -0,0 +1,21 @@
+import ISeabotConfig from "./ISeabotConfig";
+
+const defaultConfig: ISeabotConfig = {
+    userVoiceChannels: {
+        groupId: "",
+        triggerChannelId: "",
+    },
+    autoDeleteMessages: undefined,
+    telemetry: {
+        channels: undefined,
+        categories: undefined,
+    },
+    channelIds: undefined,
+    roleIds: {
+        moderator: "",
+        everyone: "",
+        premium: "",
+    },
+};
+
+export default defaultConfig;
---
+++ src/configuration/loadConfiguration.ts
@@ -0,0 +1,35 @@
+import { promises as fs } from "fs";
+
+import defaultConfig from "./defaultConfig";
+import ISeabotConfig from "./ISeabotConfig";
+
+import { Duration } from "../utils/Time/Duration";
+
+export default async function loadConfiguration(path: string): Promise<ISeabotConfig> {
+    path = `${path}\\seabotConfig.json`;
+    console.log(`Loading configuration file from "${path}"...`);
+    let configuration = null;
+
+    try {
+        await fs.access(`${path}`);
+        configuration = JSON.parse((await fs.readFile(path)).toString()) as ISeabotConfig;
+        if (configuration.autoDeleteMessages) {
+            configuration.autoDeleteMessages.channels.forEach((channel) => {
+                if (channel.timeBeforeClearing) {
+                    channel.timeBeforeClearing = new Duration(channel.timeBeforeClearing);
+                }
+            });
+        }
+    } catch (error) {
+        /*
+         * This will only catch instances where the configuration file is not valid JSON or not present.
+         * When the configuration file schema is in a more final state, a full type guard should be established
+         * to validate the loaded configuration file.
+         */
+        console.warn("Configuration file not found, or is malformed. Continuing with default configuration...");
+        configuration = defaultConfig;
+    }
+
+    return configuration;
+}
+
