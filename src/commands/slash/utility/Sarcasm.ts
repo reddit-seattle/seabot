1b66d526137f3da0b214d2fbc1c2fc2d5b4c4cef
Dameon Laird <dameonlair@gmail.com>
2022-12-06 13:05:06 -0800

    Oh hey discord's emoji class has toString

--- src/commands/slash/utility/Sarcasm.ts
+++ src/commands/slash/utility/Sarcasm.ts
@@ -2,25 +2,19 @@ import { SlashCommandBuilder } from "discord.js";
 
 import SlashCommand from "../SlashCommand";
 
-import { toSarcasticCase } from "../../../utils/helpers";
 import { discordBot } from "../../../server";
+import { toSarcasticCase } from "../../../utils/helpers";
 
 export default new SlashCommand({
     name: "sarcasm",
     description: "Make text sArCaStIc",
-    builder: new SlashCommandBuilder()
-        .addStringOption((option) => {
-            return option.setRequired(true).setName("text").setDescription(toSarcasticCase("The text to sarcasticize"));
-        }),
+    builder: new SlashCommandBuilder().addStringOption((option) => {
+        return option.setRequired(true).setName("text").setDescription(toSarcasticCase("The text to sarcasticize"));
+    }),
     execute: (interaction) => {
         const string = interaction.options.getString("text") ?? null;
-        const spongeEmoji = discordBot.client.emojis.cache.find((x) => x.name === "stupidsponge");
-        let spongeText: string;
-        if (!spongeEmoji) {
-            spongeText = "";
-        } else {
-            spongeText = `<${spongeEmoji.name}:${spongeEmoji.id}>`;
-        }
+        const emoji = discordBot.client.emojis.cache.find((x) => x.name === "stupidsponge");
+        const spongeText = emoji?.toString() ?? "";
         string && interaction.reply(`${spongeText} ${toSarcasticCase(string)} ${spongeText}`);
     },
 });
