8a4fec43edcaaada4119027c7723e62c23b783a1
Dameon Laird <dameonlair@gmail.com>
2022-12-07 09:14:31 -0800

    Fixed RJ - changes to options had broken him. Sorted RJ strings Also re-added quotes Prettier had removed.

--- src/commands/slash/rj/RJSays.ts
+++ src/commands/slash/rj/RJSays.ts
@@ -6,26 +6,26 @@ import SlashCommand from "../SlashCommand";
 import { discordBot } from "../../../server";
 
 const RJStrings: { [id: string]: string } = {
-    sad: `<rj4>`,
-    hurts: `<rj4>`,
+    "sad": `<rj4>`,
+    "hurts": `<rj4>`,
     "shake it off": `<partyrj><tsktsk>`,
-    no: `<rj><tsktsk>`,
-    disapproves: `<rj><tsktsk>`,
+    "no": `<rj><tsktsk>`,
+    "disapproves": `<rj><tsktsk>`,
     "tsk tsk": `<rj><tsktsk>`,
-    chancla: `<rj><lachancla>`,
+    "chancla": `<rj><lachancla>`,
     "oh no": `<ohno><rj>`,
     "double oh no": `<ohno><rj><ohnoreverse>`,
-    harold: `<rj3>`,
+    "harold": `<rj3>`,
     "is watching": `<rj3>`,
-    watching: `<rj3>`,
-    suit: `<rj>\n👔`,
-    party: `<partyrj>`,
-    dance: `<partyrj>`,
+    "watching": `<rj3>`,
+    "suit": `<rj>\n👔`,
+    "party": `<partyrj>`,
+    "dance": `<partyrj>`,
     "finger guns": `<rj><fingerguns>`,
-    ayy: `<rj><fingerguns>`,
-    zoop: `<rj><fingerguns>`,
-    hockey: `<rj>\n<krakenjersey>`,
-    kraken: `<rj>\n<krakenjersey>`,
+    "ayy": `<rj><fingerguns>`,
+    "zoop": `<rj><fingerguns>`,
+    "hockey": `<rj>\n<krakenjersey>`,
+    "kraken": `<rj>\n<krakenjersey>`,
 };
 
 function textToEmojis(text: string) {
@@ -54,9 +54,8 @@ export default new SlashCommand({
             .setDescription("makes funny little RJ emotes")
             .addStringOption((option) => {
                 option.setName("emote").setDescription("which emote would you like");
-                Object.keys(RJStrings).forEach((emoji) => {
-                    option.addChoices({ name: emoji, value: RJStrings[emoji] });
-                });
+                const sortedChoices = Object.keys(RJStrings).sort();
+                option.addChoices(...sortedChoices.map((choice) => { return { name: choice, value: RJStrings[choice] }}));
                 return option;
             }),
     execute: (interaction) => {
@@ -67,7 +66,7 @@ export default new SlashCommand({
             interaction.reply(textToEmojis(options[val]));
             return;
         } else {
-            interaction.reply(RJStrings?.[emote.toLowerCase()] ?? "RJ does not know that command");
+            interaction.reply(emote ? textToEmojis(emote) : "RJ does not know that command");
         }
     },
 });
