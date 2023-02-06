17b8cfda3af356511c3e15c24503fdfa75d28ccb
burn-piano-island <36089446+burn-piano-island@users.noreply.github.com>
2023-02-06 10:49:43 -0800

    WIP audio stuff

--- src/functions/voiceChannelManagement.ts
+++ src/functions/voiceChannelManagement.ts
@@ -5,8 +5,11 @@ import {
 } from "discord.js";
 import { configuration } from "../server";
 import { Environment, VoiceConstants } from "../utils/constants";
+import { joinVoiceChannel,createAudioPlayer, NoSubscriberBehavior, createAudioResource, generateDependencyReport, getVoiceConnection   } from '@discordjs/voice';
+import { join } from "path";
 
 const { Permissions } = VoiceConstants;
+console.log(generateDependencyReport());
 
 export const handleVoiceStatusUpdate = async (
   oldState: VoiceState,
@@ -29,6 +32,61 @@ export const handleVoiceStatusUpdate = async (
     ) {
     await deleteEmptyMemberVoiceChannel(oldState);
   }
+
+  // if user joins hangouts and i'm not there
+  let hangouts = '370945003566006276';
+  
+  // dev
+  hangouts = '994014273074372630';
+
+  /**
+   * Next steps
+   * Connect azure storage blob as file share, find mount URL
+   * append users mount URL
+   * add custom config entry for audio clips
+   * dict{[userid: string]: file_name: string}
+   * 
+   * logic:
+   *  if a premium user joins hangouts
+   *    join the channel if i'm not already there
+   *    lookup their song
+   *    play it
+   *  if a premium user leaves hangouts and i'm there
+   *    lookup their outro
+   *    play it
+   *  
+   */
+ 
+  const connection = getVoiceConnection(newState.guild.id);
+  
+  if(newState.channel) {
+    const connection = joinVoiceChannel({
+      channelId: newState.channel.id,
+      guildId: newState.guild.id,
+      adapterCreator: newState.guild.voiceAdapterCreator,
+    });
+
+    const audioPlayer = createAudioPlayer({
+      behaviors: {
+        noSubscriber: NoSubscriberBehavior.Pause
+      }
+    });
+    audioPlayer.on('error', error => {
+      console.error('Error:', error.message);
+    });
+    const subscription = connection.subscribe(audioPlayer);
+
+    if (subscription) {
+
+      const resource = createAudioResource(join(__dirname, '../../noise.mp3'));
+      resource.playStream.on('error', error => {
+        console.error('Error:', error.message);
+      });
+
+      audioPlayer.play(resource);
+    }
+  }
+
 };
 
 export const createVoiceChannelForMember = async (state: VoiceState) => {
