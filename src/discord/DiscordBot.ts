c858b9b0b782017a37158bcee4564404f8d5ca88
burn-piano-island <36089446+burn-piano-island@users.noreply.github.com>
2023-12-30 16:34:54 -0800

    Minor tweaks
    Announce user's name who joined and then immediately left
    
    Update node build versions to 18

--- src/discord/DiscordBot.ts
+++ src/discord/DiscordBot.ts
@@ -132,9 +132,9 @@ export default class DiscordBot {
     if (!member.joinedAt) return;
 
     if (Date.now() - member.joinedAt.getTime() < minutesToMilliseconds(5)) {
-      const { guild } = member;
-      guild?.systemChannel?.send(
-        "https://media.giphy.com/media/fDO2Nk0ImzvvW/giphy.gif"
+      const { guild, nickname } = member;
+      guild?.systemChannel?.send(`Thanks for stopping by, ${nickname}
+        https://media.giphy.com/media/fDO2Nk0ImzvvW/giphy.gif`
       );
     }
   }
