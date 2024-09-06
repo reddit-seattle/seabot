a4c2585d9350690038ef7a13ae464a28488734d0
giantorth <56210456+giantorth@users.noreply.github.com>
2024-09-06 10:24:10 -0700

    Update AutoRespond.ts

--- src/commands/messageContent/AutoRespond.ts
+++ src/commands/messageContent/AutoRespond.ts
@@ -25,9 +25,9 @@ const responseMap = new Map<string | RegExp, AutoResponse>([
   [/^SEA$/im, { message: "HAWKS!" }],
   [/(tbf|to be fair)/i, { message: Strings.letterkennyGif, chance: 0.33 }],
   [/(\s|^)eggs?/i, {reaction: "🥚", chance: 0.2}],
-  [/nytimes.com/i, { reaction: () => emojiFromName("poop"), trim: true }],
-  [/washingtonpost.com/i, { reaction: () => emojiFromName("poop"), trim: true }],
-  [/wsj.com/i, { reaction: () => emojiFromName("poop"), trim: true }]
+  [/nytimes.com/i, { reaction: "💩"}],
+  [/washingtonpost.com/i, { reaction: "💩" }],
+  [/wsj.com/i, { reaction: "💩" }]
 ]);
 
 function emojiFromName(emojiName: string): GuildEmoji {
